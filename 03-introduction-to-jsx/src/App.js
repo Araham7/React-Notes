94a169b2b4847f71a72392d3d0cde63bd4adba72
Araham7 <arahamabeddin7@gmail.com>
2024-12-11 00:44:18 +0530

    Add 03

---
+++ 03-introduction-to-jsx/src/App.js
@@ -0,0 +1,24 @@
+// (1).
+// function App() {
+//   return (
+//     <h1>Hello World!</h1>
+//   );
+// }
+
+// (2). 
+const App = ()=>{
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <button className="btn">
+      <span>
+        Submit
+      </span>
+    </button>
+    </>
+  )
+}
+
+export default App;
+
+
